d05edf1a1bebd1ba80bab6a2357e0dac88cc52e0
M-RUSLAN-DURRANI <ruslandurrani907@gmail.com>
2022-07-16 12:56:01 +0500

    update

--- Stacks/DOM/DrumKit/index.js
+++ Stacks/DOM/DrumKit/index.js
@@ -3,23 +3,36 @@ for (var i = 0; i < document.querySelectorAll("button.drum").length; i++) {
 
   document.querySelectorAll("button.drum")[i].addEventListener("click",function(){
     // console.log(this.innerHTML);
-    switch(this.innerHTML){
-      case "w":new Audio("sounds/crash.mp3").play();break;
-      case "a":new Audio("sounds/kick-bass.mp3").play();break;
-      case "s":new Audio("sounds/snare.mp3").play();break;
-      case "d":new Audio("sounds/tom-1.mp3").play();break;
-      case "j":new Audio("sounds/tom-2.mp3").play();break;
-      case "k":new Audio("sounds/tom-3.mp3").play();break;
-      case "l":new Audio("sounds/tom-4.mp3").play();break;
-      
-    }
+    handleSound(this.innerHTML);
+    animateButton(this.innerHTML);
     // playSound(i);
 
   });
 
 }
 
-// function playSound(i){
-//   var a = new Audio("/Users/russi7kd/github/MERN/Stacks/DOM/DrumKit/sounds/tom-"+i+".mp3");
-//   a.play();
-// }
+document.addEventListener("keypress", function(e){
+  handleSound(e.key);
+  animateButton(e.key);
+
+});
+
+function handleSound(i){
+  switch(i){
+    case "w":new Audio("sounds/crash.mp3").play();break;
+    case "a":new Audio("sounds/kick-bass.mp3").play();break;
+    case "s":new Audio("sounds/snare.mp3").play();break;
+    case "d":new Audio("sounds/tom-1.mp3").play();break;
+    case "j":new Audio("sounds/tom-2.mp3").play();break;
+    case "k":new Audio("sounds/tom-3.mp3").play();break;
+    case "l":new Audio("sounds/tom-4.mp3").play();break;
+  }
+}
+
+
+function animateButton(i){
+  document.querySelector("."+i).classList.add("pressed");
+  setTimeout(function(){
+    document.querySelector("."+i).classList.remove("pressed");
+  },100);
+}
